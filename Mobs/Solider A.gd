1fb29d8e5aeb58d9db40c3f0b2bca58b24b54299
Peter Hymøller <peter.hymoller@danfoss.com>
2023-06-13 10:37:08 +0200

    update fully working

--- Mobs/Solider A.gd
+++ Mobs/Solider A.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
 @export var speed = 10
-
 var Health = 10
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_parent().set_progress(get_parent().get_progress() +speed+delta)
+	get_parent().set_progress(get_parent().get_progress() + speed*delta)
+	
 	if get_parent().get_progress_ratio() == 1:
-		queue_free()
+		death()
+		Game.Health -= 1
+		
 	if Health <= 0:
-		get_parent().get_parent().queue_free()
+		death()
+		Game.Gold += 1
+	
+func death():
+	get_parent().get_parent().queue_free()
--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -4,47 +4,53 @@ extends Panel
 var currTile
 
 func _on_gui_input(event):
-	var tempTower = tower.instantiate()
-	if event is InputEventMouseButton and event.button_mask == 1:
-		#left click down
-		add_child(tempTower)
-		#tempTower.global_position = event.global_position
-		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
-		tempTower.scale = Vector2(0.32,0.32)
-	
-	elif event is InputEventMouseMotion and event.button_mask == 1:
-		#left click down drag
-		if get_child_count() > 1:
-			get_child(1).global_position = event.global_position
+	if Game.Gold >= 10:
+		var tempTower = tower.instantiate()
+		if event is InputEventMouseButton and event.button_mask == 1:
+			#left click down
+			add_child(tempTower)
 			
-			var mapPath = get_tree().get_root().get_node("Main/TileMap")
-			var tile = mapPath.local_to_map(get_global_mouse_position())
-			currTile = mapPath.get_cell_atlas_coords(0, tile, false)
-			#var targets = get_child(1).get_node("TowerDetector").get_overlapping_bodies()
-			if (currTile == Vector2i(4,5)):
-				#if (targets.size() > 1):
-					get_child(1).get_node("Area").modulate = Color(255, 0, 0, 0.3)
-				#else:
-					get_child(1).get_node("Area").modulate = Color(0 ,255 ,0, 0.3)
-			else:
-				get_child(1).get_node("Area").modulate = Color(255 ,0 ,0, 0.3)
-	
-	elif event is InputEventMouseButton and event.button_mask == 0:
-		#left click up
-		if event.global_position.x >= 2944:
+			tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+			tempTower.global_position = event.global_position
+			tempTower.scale = Vector2(0.32,0.32)
+		
+		elif event is InputEventMouseMotion and event.button_mask == 1:
+			#left click down drag
 			if get_child_count() > 1:
-				get_child(1).queue_free()
+				get_child(1).global_position = event.global_position
+				
+				var mapPath = get_tree().get_root().get_node("Main/TileMap")
+				var tile = mapPath.local_to_map(get_global_mouse_position())
+				currTile = mapPath.get_cell_atlas_coords(0, tile, false)
+				var targets = get_child(1).get_node("TowerDetector").get_overlapping_bodies()
+				
+				if (currTile == Vector2i(4,5)):
+					if (targets.size() > 1):
+						get_child(1).get_node("Area").modulate = Color(255, 0, 0, 0.3)
+					else:
+						get_child(1).get_node("Area").modulate = Color(0 ,255 ,0, 0.3)
+				else:
+					get_child(1).get_node("Area").modulate = Color(255 ,0 ,0, 0.3)
+		
+		elif event is InputEventMouseButton and event.button_mask == 0:
+			#left click up
+			if event.global_position.x >= 2944:
+				if get_child_count() > 1:
+					get_child(1).queue_free()
+			else:
+				if get_child_count() > 1:
+					get_child(1).queue_free()
+				if currTile == Vector2i(4,5):
+					var path = get_tree().get_root().get_node("Main/Towers")
+					var targets = get_child(1).get_node("TowerDetector").get_overlapping_bodies()
+					if (targets.size() < 1):
+						path.add_child(tempTower)
+						tempTower.global_position = event.global_position
+						tempTower.get_node("Area").hide()
+					Game.Gold -= 10
+			
 		else:
 			if get_child_count() > 1:
 				get_child(1).queue_free()
-			if currTile == Vector2i(4,5):
-				var path = get_tree().get_root().get_node("Main/Towers")
-				path.add_child(tempTower)
-				tempTower.global_position = event.global_position
-				tempTower.get_node("Area").hide()
-		
-	else:
-		if get_child_count() > 1:
-			get_child(1).queue_free()
 
 
